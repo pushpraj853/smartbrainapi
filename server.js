e00b8dc1512d9e64f4fda8803eb7def1722c6859
pushpraj853 <pushpraj853@gmail.com>
2022-05-16 09:56:54 +0530

    updating database host

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 const db  = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-rugged-44209',
     port : 5432,
     user : 'postgres',
     password : '123',
